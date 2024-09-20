40995fa21f9d37c806a377a9f0020405df7d23f3
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-09-20 21:26:57 +0200

    tasked-app init

---
+++ backend/src/main/java/com/kte/tasked/TaskedApplication.java
@@ -0,0 +1,13 @@
+package com.kte.tasked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskedApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kte/tasked/TaskedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kte.tasked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
